93e396f39a1edbed18ac4e8b78e08e7fd0b707ad
BatMaxou <maximebatista.lycee@gmail.com>
2025-08-01 19:06:07 +0200

    :wrench: Disable tree change focus

--- nvim/lua/configs/tree.lua
+++ nvim/lua/configs/tree.lua
@@ -8,10 +8,10 @@ return {
       custom = { "^\\.git$" },
     },
     disable_netrw = true,
-    hijack_cursor = true,
-    sync_root_with_cwd = true,
+    hijack_cursor = false,
+    sync_root_with_cwd = false,
     update_focused_file = {
-      enable = true,
+      enable = false,
       update_root = false,
     },
     view = {
