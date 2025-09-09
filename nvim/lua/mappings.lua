05dc7e146b75ae1394c5ca006395dae342f37cc0
BatMaxou <maximebatista.lycee@gmail.com>
2025-09-09 12:19:21 +0200

    :sparkles: Start / end word motion mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -21,6 +21,8 @@ map("n", "<leader>rn", "<cmd>set rnu!<CR>", { desc = "Toggle relative number" })
 
 map("n", "à", "0", { desc = "Move to the beginning of the line" })
 map({ "n", "v" }, "<S-b>", "e", { desc = "End Word" })
+map({ "n", "v" }, "<S-j>", "b", { desc = "Start Word" })
+map({ "n", "v" }, "<S-l>", "e", { desc = "End Word" })
 
 map("n", "<leader>,", "$a,", { desc = "Add ',' at the end of a line" })
 map("n", "<leader>;", "$a;<esc>", { desc = "Add ';' at the end of a line" })
