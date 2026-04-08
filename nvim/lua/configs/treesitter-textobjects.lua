d63888688ba78af7c266ab811bcf28aa415bbb32
BatMaxou <maximebatista.lycee@gmail.com>
2026-04-08 13:55:48 +0200

    :bug: Migrate to new version of Treesitter

--- nvim/lua/configs/treesitter-textobjects.lua
+++ nvim/lua/configs/treesitter-textobjects.lua
@@ -3,41 +3,38 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          include_surrounding_whitespace = false,
-          keymaps = {
-            ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
-            ["af"] = { query = "@function.outer", desc = "Select a function" },
-            ["ac"] = { query = "@class.outer", desc = "Select a class" },
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-            ["a:"] = { query = "@comment.outer", desc = "Select a block of comment" },
-            ["i:"] = { query = "@comment.inner", desc = "Select inner part of a comment block" },
-            ["al"] = { query = "@loop.outer", desc = "Select a loop" },
-            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            ["(j"] = { query = "@function.outer", desc = "Go to next function start" },
-            ["'j"] = { query = "@loop.outer", desc = "Go to next loop start" },
-            ["-j"] = { query = "@parameter.inner", desc = "Go to next parameter" },
-          },
-          goto_previous_start = {
-            ["(k"] = { query = "@function.outer", desc = "Go to previous function start" },
-            ["'k"] = { query = "@loop.outer", desc = "Go to previous loop start" },
-            ["-k"] = { query = "@parameter.inner", desc = "Go to previous parameter" },
-          },
-          goto_next_end = {},
-          goto_previous_end = {},
-        },
+  branch = "main",
+  config = {
+    select = {
+      enable = true,
+      lookahead = true,
+      include_surrounding_whitespace = false,
+      keymaps = {
+        ["if"] = { query = "@function.inner", desc = "Select inner part of a function" },
+        ["af"] = { query = "@function.outer", desc = "Select a function" },
+        ["ac"] = { query = "@class.outer", desc = "Select a class" },
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+        ["a:"] = { query = "@comment.outer", desc = "Select a block of comment" },
+        ["i:"] = { query = "@comment.inner", desc = "Select inner part of a comment block" },
+        ["al"] = { query = "@loop.outer", desc = "Select a loop" },
+        ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
       },
-    })
-  end,
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        ["(j"] = { query = "@function.outer", desc = "Go to next function start" },
+        ["'j"] = { query = "@loop.outer", desc = "Go to next loop start" },
+        ["-j"] = { query = "@parameter.inner", desc = "Go to next parameter" },
+      },
+      goto_previous_start = {
+        ["(k"] = { query = "@function.outer", desc = "Go to previous function start" },
+        ["'k"] = { query = "@loop.outer", desc = "Go to previous loop start" },
+        ["-k"] = { query = "@parameter.inner", desc = "Go to previous parameter" },
+      },
+      goto_next_end = {},
+      goto_previous_end = {},
+    },
+  },
 }
--- nvim/lua/configs/treesitter.lua
+++ nvim/lua/configs/treesitter.lua
@@ -1,57 +1,61 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  branch = 'main',
+  main = 'nvim-treesitter',
+  build = ':TSUpdate',
+  opts = {
+    auto_install = true,
+  },
+  init = function()
+    -- Incremental selection using Neovim 0.12 built-in treesitter textobjects (an/in)
+    vim.keymap.set('n', '<Enter>', 'van', { remap = true, desc = 'Init treesitter selection' })
+    vim.keymap.set('x', '<Enter>', 'an', { remap = true, desc = 'Expand treesitter selection' })
+    vim.keymap.set('x', '<BS>', 'in', { remap = true, desc = 'Shrink treesitter selection' })
 
-    configs.setup({
-      ensure_installed = {
-        "bash",
-        "css",
-        "dockerfile",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "nginx",
-        "php",
-        "phpdoc",
-        "python",
-        "regex",
-        "sql",
-        "twig",
-        "typescript",
-        "tsx",
-        "vim",
-        "vimdoc",
-        "vue",
-        "xml",
-        "yaml",
-      },
-      sync_install = false,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<Enter>",
-          node_incremental = "<Enter>",
-          scope_incremental = false,
-          node_decremental = "<Backspace>",
-        },
-      },
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
+    local ensureInstalled = {
+      "bash",
+      "css",
+      "dockerfile",
+      "git_config",
+      "git_rebase",
+      "gitattributes",
+      "gitcommit",
+      "gitignore",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "luadoc",
+      "markdown",
+      "markdown_inline",
+      "nginx",
+      "php",
+      "phpdoc",
+      "python",
+      "regex",
+      "sql",
+      "twig",
+      "typescript",
+      "tsx",
+      "vim",
+      "vimdoc",
+      "vue",
+      "xml",
+      "yaml",
+    }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    require('nvim-treesitter').install(parsersToInstall)
   end,
 }
